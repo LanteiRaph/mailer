d1755050a592ad273585d73b9df0b6aeefee22be
lanteiRaph <santamulantei8@gmail.com>
2022-07-04 10:57:44 +0300

    Fixed mailler function

--- email-script.php
+++ email-script.php
@@ -1,15 +1,25 @@
 <?php
-if (isset($_POST['submit'])) {
+
+if(isset($_POST)){
+
+    //Extract the values{name, email, to,form and msg with subject} from the request 
     $Name = $_POST['Name'];
     $Email = $_POST['Email'];
     $subjectName = $_POST['subject'];
+    //Extract the msg to send 
     $message = $_POST['message'];
-
-    $to = $toEmail;
+    //
+    //Compile the massege to send.Formated as HMTl for broswer
+    $to = 'beliorotich@gmail.com';
+    //Message to send
     $subject = $subjectName;
+    //
+    //Compile haeders for the request.
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: '.$Name'<'.$Email.'>' . "\r\n".'Reply-To: '.$Name."\r\n" . 'X-Mailer: PHP/' . phpversion();
+    $headers .= 'From: '.$Name.'<'.$Email.'>' . "\r\n".'Reply-To: '.$Name."\r\n" . 'X-Mailer: PHP/' . phpversion();
+    //
+    //Format Htl respose value
     $message = '<!doctype html>
 			<html lang="en">
 			<head>
@@ -29,7 +39,15 @@ if (isset($_POST['submit'])) {
 			</body>
 			</html>';
     $result = @mail($to, $subject, $message, $headers);
-
+    //
+    if($result){
+        echo 'True';
+    }else{
+        echo 'false';
+    }
+    echo $result;
     echo '<script>alert("Email sent successfully !")</script>';
-    echo '<script>window.location.href="index.php";</script>';
+    // echo '<script>window.location.href="index.php";</script>';
 }
+
+?>
